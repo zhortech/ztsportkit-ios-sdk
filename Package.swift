29b7ba7dfd70fa99bc561bda024013c2686c80ee
Zhortech <p.shpak@zhortech.com>
2021-07-23 15:41:00 +0300

    SPM support

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "ZTSportKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "ZTSportKit",
+            targets: ["ZTSportKitSDK"]
+        )
+    ],
+    dependencies: [
+        .package(
+            name: "ZTCoreKit", 
+            url: "https://github.com/zhortech/ztcorekit-ios-sdk.git",
+            from: "1.1.2"
+        )
+    ],
+    targets: [
+        .target(
+            name: "ZTSportKitSDK",
+            dependencies: [
+                .product(
+                    name: "ZTCoreKit",
+                    package: "ZTCoreKit"
+                ),
+                .target(
+                    name: "ZTSportKit"
+                )
+            ],
+            path: "Sources/ZTSportKitSDK"
+        ),
+        .binaryTarget(
+            name: "ZTSportKit",
+            path: "Sources/ZTSportKit.xcframework"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
